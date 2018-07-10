04b12826af54974b9904821cfdf88161e368c6f4
Ananya <ananya1703@gmail.com>
2018-07-10 21:54:00 +0530

    Files

---
+++ AnurekhaSharma/js/controls.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#hi").hide();
+    //alert(9)
+});
+$(".publications").click(function(){
+	//alert(9090)
+    $("#hi").show();
+});
+
+$(".aboutMe").click(function(){
+	 $("#hi").hide();
+});
