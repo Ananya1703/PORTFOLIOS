e8b8c6e9d98e9adf68202e87d66fe18f505e212f
Ananya <Ananya1703@users.noreply.github.com>
2022-06-27 11:31:26 +0530

    changes

--- AnurekhaSharma/js/controls.js
+++ AnurekhaSharma/js/controls.js
@@ -6,10 +6,31 @@ $(document).ready(function(){
 $(".publications").click(function(){
 	//alert(9090)
     $("#info").hide();
+    $("#researchWork").hide();
     $("#publications").show();
 });
 
 $(".info").click(function(){
 	 $("#info").show();
 	 $("#publications").hide();
+     $("#researchWork").hide();
+});
+
+$(document).ready(function(){
+    $("#hi").hide();
+    $("#researchWork").hide();
+    //alert(9)
+});
+$(".researchWork").click(function(){
+    //alert(9090)
+    $("#info").hide();
+    $("#publications").hide();
+    $("#researchWork").show();
+    //alert(10)
+});
+
+$(".info").click(function(){
+     $("#info").show();
+     $("#researchWork").hide();
+     $("#publications").hide();
 });
